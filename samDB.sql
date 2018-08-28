33edb677868350018dfc72df179bcf3a22cae1ea
wacero <wacero@mail.igepn.edu.ec>
2018-08-28 13:22:34 -0500

    Update samDB.sql

--- samDB.sql
+++ samDB.sql
@@ -7,7 +7,7 @@ GRANT ALL PRIVILEGES ON samDB.* to 'sc3sam'@'localhost' IDENTIFIED BY '****';
 USE samDB; 
 
 
-CREATE TABLE events ( eventID VARCHAR(80), latitude DOUBLE(8,44), longitude DOUBLE(8,4), description VARCHAR(200), magVal DOUBLE(6,3), magType VARCHAR(20),timestampSec DATETIME, timestampNow DATETIME, depth DOUBLE(12,4), status VARCHAR(20), revision INT(10), localizacion VARCHAR(200), horaLocal DATETIME, PRIMARY KEY (eventID) ); 
+CREATE TABLE events ( eventID VARCHAR(80), latitude DOUBLE(8,4), longitude DOUBLE(8,4), description VARCHAR(200), magVal DOUBLE(6,3), magType VARCHAR(20),timestampSec DATETIME, timestampNow DATETIME, depth DOUBLE(12,4), status VARCHAR(20), revision INT(10), localizacion VARCHAR(200), horaLocal DATETIME, PRIMARY KEY (eventID) ); 
 
 
 
